edfe84df49b7eb50cfaa9a4291ce3cfccaae4630
MrKong <nuttakornpe@gmail.com>
2022-02-09 18:32:15 +0700

    node_challenges

--- index.js
+++ index.js
@@ -99,23 +99,12 @@ const init = async () => {
       }
     });
 
-    server.route({
-      method: 'GET',
-      path: '/error',
-      handler: (request, h) => {
-
-          throw Boom.notFound('Page not found');
-      }
-    });
-
     // Challenges_1
-
     server.route({
       method: 'GET',
       path: '/path1',
       handler: (request, h) => {
         let newJsonFormat = [];
-        console.log(newJsonFormat);
 
         // Get keys level by desc
         const levels = Object.keys(appendix1Input);
@@ -128,12 +117,12 @@ const init = async () => {
           } else {   
             const childrenList = newJsonFormat;
             childrenList.forEach(child => {
-              const parent = parentList.find(p => p.id === child.parent_id)
+              const parent = parentList.find(p => p.id === child.parent_id);
               if (parent) {
-                parent.children.push(child)
+                parent.children.push(child);
               } else {
-                console.log(child)
-                console.log('!!!! Error not found parent !!!!')
+                console.log('!!!! Error not found parent !!!!');
+                console.log('Error data : ', child);
                 throw Boom.badData(`Data id ${child.id} not found parent [patent_id: ${child.parent_id}]. Please check your data`);
               }
             })
@@ -147,108 +136,6 @@ const init = async () => {
         };
       }
     })
-
-    // server.route({
-    //   method: 'GET',
-    //   path: '/path1',
-    //   handler: (request, h) => {
-    //     let newJsonFormat = [];
-    //     const keys = Object.keys(appendix1Input);
-    //     keys.sort((a, b) => a-b);
-    //     keys.forEach(k => {
-    //       const objByLevel = appendix1Input[k];
-    //       if (newJsonFormat.length === 0) {
-    //         newJsonFormat.push(...objByLevel);
-    //       } else if (+k === 1) {
-    //         console.log(`objByLevel : `, objByLevel);
-    //         objByLevel.forEach(child => {
-    //           const parent = newJsonFormat.find(p => p.id === child.parent_id)
-    //           parent.children.push(child);
-    //           console.log(`child : `, child);
-    //           console.log(`parent : `, parent);
-    //           console.log(' ------- ')
-    //         })
-    //       } else {
-    //         console.log(k);
-    //       }
-    //     });
-    //     return {
-    //       keys: keys,
-    //       newJsonFormat
-    //     };
-    //   }
-    // })
-
-    // server.route({
-    //   method: 'GET',
-    //   path: '/path1',
-    //   handler: (request, h) => {
-    //       const keys = Object.keys(appendix1Input);
-    //       let sourceJsonData = [];
-    //       let newFormat = [];
-
-    //       // Deconstruct source data
-    //       keys.forEach(k => {
-    //         // console.log(`${k} | ${appendix1Input[k].length}`)
-    //         for (let i = 0; i < appendix1Input[k].length; i++) {
-    //           const { id, title, level, children, parent_id } = appendix1Input[k][i]
-    //           sourceJsonData.push({
-    //             id,
-    //             title,
-    //             level,
-    //             children,
-    //             parent_id
-    //           })
-    //         }
-    //       })
-
-    //       // Get total level and sort asc
-    //       const totalLevel = [...new Set(sourceJsonData.map(tL => tL.level))]
-    //       totalLevel.sort((a, b) => a - b)
-
-    //       totalLevel.forEach(level => {
-    //         const objEachLevel = sourceJsonData.filter(d => d.level === level)
-    //         objEachLevel.forEach(obj => {
-    //           if (level === 0) {
-    //             newFormat.push(obj)
-    //           } 
-    //         })
-    //         // newFormat.push(objEachLevel)
-    //       })
-
-    //       return {
-    //         keys: keys,
-    //         newFormat,
-    //         totalLevel,
-    //         sourceJsonData,
-    //       };
-
-    //       // // Count parent
-    //       // const totalParentId = [...new Set(sourceJsonData.map(tp => tp.parent_id))]
-    //       // let cntParent = [];
-    //       // totalParentId.forEach(p => { 
-    //       //   const obj = sourceJsonData.filter(d => d.parent_id === p)
-    //       //   cntParent.push({
-    //       //     parentId: p,
-    //       //     cnt: obj.length
-    //       //   })
-    //       // })
-    //       // // Sort asc
-    //       // const sortParentId = cntParent.sort((a, b) => a.cnt - b.cnt)
-
-    //       // Set new json format
-    //       // const mainParent = sourceJsonData.filter(mp => { return mp.parent_id === null })
-    //       // totalParentId.forEach(pid => {
-    //       //   if (pid) {
-    //       //     const parentObj = sourceJsonData.find(d => { return d.id === pid })
-    //       //     const childArray = sourceJsonData.find(d => { return d.id === pid })
-    //       //     console.log(pid);
-    //       //     console.log(parentObj);
-    //       //     console.log(' ------- ')
-    //       //   }
-    //       // })
-    //   }
-    // });
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
