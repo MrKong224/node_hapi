4450f7bde4645829b99292504c03ba6dd42b1986
MrKong <nuttakornpe@gmail.com>
2022-02-09 18:42:53 +0700

    node_challenges: post api path1

--- index.js
+++ index.js
@@ -3,86 +3,6 @@
 const Hapi = require('@hapi/hapi');
 const Boom = require('@hapi/boom');
 
-const appendix1Input = {"0":
-[{"id": 10,
-  "title": "House",
-  "level": 0,
-  "children": [],
-  "parent_id": null}],
-"1":
-[{"id": 12,
-  "title": "Red Roof",
-  "level": 1,
-  "children": [],
-  "parent_id": 10},
- {"id": 18,
-  "title": "Blue Roof",
-  "level": 1,
-  "children": [],
-  "parent_id": 10},
- {"id": 13,
-  "title": "Wall",
-  "level": 1,
-  "children": [],
-  "parent_id": 10}],
-"2":
-[{"id": 17,
-  "title": "Blue Window",
-  "level": 2,
-  "children": [],
-  "parent_id": 12},
- {"id": 16,
-  "title": "Door",
-  "level": 2,
-  "children": [],
-  "parent_id": 13},
- {"id": 15,
-  "title": "Red Window",
-  "level": 2,
-  "children": [],
-  "parent_id": 12},
-  {"id": 19,
-  "title": "SNK WRONG PARENT",
-  "level": 2,
-  "children": [],
-  "parent_id": 10}]};
-
-const appendix2Result = [{"id": 10,
-"title": "House",
-"level": 0,
-"children":
- [{"id": 12,
-   "title": "Red Roof",
-   "level": 1,
-   "children":
-    [{"id": 17,
-      "title": "Blue Window",
-      "level": 2,
-      "children": [],
-      "parent_id": 12},
-     {"id": 15,
-      "title": "Red Window",
-      "level": 2,
-      "children": [],
-      "parent_id": 12}],
-   "parent_id": 10},
-  {"id": 18,
-   "title": "Blue Roof",
-   "level": 1,
-   "children": [],
-   "parent_id": 10},
-  {"id": 13,
-   "title": "Wall",
-   "level": 1,
-   "children":
-    [{"id": 16,
-      "title": "Door",
-      "level": 2,
-      "children": [],
-      "parent_id": 13}],
-   "parent_id": 10}],
-"parent_id": null}]
-
 const init = async () => {
 
     const server = Hapi.server({
@@ -100,6 +20,7 @@ const init = async () => {
     });
 
     // Challenges_1
+/*
     server.route({
       method: 'GET',
       path: '/path1',
@@ -130,12 +51,46 @@ const init = async () => {
           }
         })
 
-        return {
-          newJsonFormat,
-          appendix2Result
-        };
+        return newJsonFormat;
       }
     })
+*/
+    server.route({
+      method: 'POST',
+      path: '/path1',
+      handler: function (request, h) {
+  
+        const payload = request.payload;
+        let newJsonFormat = [];
+
+        // Get keys level by desc
+        const levels = Object.keys(payload);
+        levels.sort((a, b) => b - a);
+
+        levels.forEach(level => {
+          const parentList = payload[level];
+          if (newJsonFormat.length === 0) {
+            newJsonFormat.push(...parentList);
+          } else {   
+            const childrenList = newJsonFormat;
+            childrenList.forEach(child => {
+              const parent = parentList.find(p => p.id === child.parent_id);
+              if (parent) {
+                parent.children.push(child);
+              } else {
+                console.log('!!!! Error not found parent !!!!');
+                console.log('Error data : ', child);
+                throw Boom.badData(`Data id ${child.id} not found parent [patent_id: ${child.parent_id}]. Please check your data`);
+              }
+            })
+            newJsonFormat = parentList;   
+          }
+        })
+
+        return newJsonFormat;
+
+      }
+  });
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
