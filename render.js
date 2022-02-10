77a1317f55eff4a463fbabbb1a76a3de278f6d67
MrKong <nuttakornpe@gmail.com>
2022-02-10 16:31:07 +0700

    node_challenges: get page from headerLink

--- render.js
+++ render.js
@@ -1,34 +1,69 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
-const Vision = require('@hapi/vision');
-const handlebars = require('handlebars');
 const Boom = require('@hapi/boom');
 const axios = require('axios');
 
 const server = Hapi.server({
-    port: 3000,
+    port: 8080,
     host: 'localhost'
 });
 
 const init = async () => {
 
     // Register
-    await server.register(Vision);
+    await server.register(require('@hapi/vision'));
 
     // Config render html path
+    const context = {
+      title: 'Github search result'
+    };
     server.views({
       engines: {
-          html: handlebars
+          html: require('handlebars')
       },
       relativeTo: __dirname,
-      path: 'public'
+      path: './public',
+      context
     });
 
     // Routes
+    // server.route({
+    //   method: 'GET',
+    //   path: '/',
+    //   handler: (req, h) => {
+    //     return h.view('index');
+    //   }
+    // });
+    const getPageNav = (headerLink) => {
+      let result = { first: null, last: null, next: null, prev: null };
+
+      const arrayLink = (headerLink).split(',').map(d => d.trim())
+      arrayLink.forEach(link => {
+        const urlLink = link.split(';').map(d => d.trim());
+        
+        // find rel type
+        const relType = urlLink[1];
+
+        // find page number
+        const pageNumber = urlLink[0].substring(urlLink[0].indexOf('&page=') + 6, urlLink[0].length - 1)
+
+        // Set result value
+        if (relType === 'rel="first"') {
+          result.first = pageNumber
+        } else if (relType === 'rel="last"') {
+          result.last = pageNumber
+        } else if (relType === 'rel="next"') {
+          result.next = pageNumber
+        } else if (relType === 'rel="prev"') {
+          result.prev = pageNumber
+        }
+      })
+      return result;
+    }
     server.route({
         method: 'GET',
-        path: '/index/{page?}',
+        path: '/page/{page?}',
         handler: async (req, h) => {
           const page = req.params.page || 1;
           try {
@@ -41,11 +76,11 @@ const init = async () => {
               console.log('Error data : ', respGithub.statusText);
               throw Boom.badData(respGithub);
             }
-            const headerLink = (respGithub.headers.link).split(',')
-            return h.view('index', {
-              title: `Table source page: ${page}`,
+            const headerLink = getPageNav(respGithub.headers.link)
+            return h.view('./index', {
               message: respGithub.data.items,
-              nav: headerLink
+              link: respGithub.headers.link,
+              nav: JSON.stringify(headerLink)
             });
           } catch (error) {
             console.log(error.response.data);
