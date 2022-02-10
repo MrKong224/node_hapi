5b2416710a722c5237104b78c0719d1adfb85c62
MrKong <nuttakornpe@gmail.com>
2022-02-10 21:29:16 +0700

    node_challenges: combine path1 and path2

--- index.js
+++ index.js
@@ -123,34 +123,6 @@ const init = async () => {
       }
     });
 
-    //Path2
-    server.route({
-      method: 'GET',
-      path: '/path2',
-      handler: async (req, h) => {
-        try {
-          const respGithub = await axios({
-            method: 'GET',
-            url: 'https://api.github.com/search/repositories?q=nodejs&per_page=10&page=2',
-          });
-          if (respGithub.status !== 200) {
-            console.log('!!!! Error during call github api !!!!');
-            console.log('Error data : ', respGithub.statusText);
-            throw Boom.badData(respGithub);
-          }
-          const headerLink = (respGithub.headers.link).split(',')
-          return {
-            status: respGithub.status,
-            headerLink,
-            data: JSON.stringify(respGithub.data)
-          }
-        } catch (error) {
-          console.log(error.response.data);
-          throw Boom.badData(JSON.stringify(error.response.data));
-        }
-      }
-    })
-
     await server.start();
     console.log('Server running on %s', server.info.uri);
 };
--- render.js
+++ render.js
@@ -11,6 +11,82 @@ const server = Hapi.server({
 
 const init = async () => {
 
+    // Path1
+    function validatePayload(payload) {
+      let flg = true;
+
+      const levels = Object.keys(payload).map(lv => +lv);
+
+      if (!levels.includes(0)) {
+        flg = false;
+        return flg;
+      }
+
+      for (let i = 0; i < levels.length; i++) {
+        const level = levels[i];
+        const dataList = payload[level];
+        const incorrectData = dataList.filter(d => d.level !== level);
+        if (incorrectData.length > 0) {
+          flg = false;
+          break;
+        }
+      }
+      
+      return flg;
+    }
+    server.route({
+      method: 'POST',
+      path: '/path1',
+      handler: (req, h) => {
+
+        const payload = req.payload;
+        if (!validatePayload(payload)) {
+          console.log('!!!! Payload is incorrect format !!!!');
+          console.log(JSON.stringify(payload));
+          throw Boom.badData(`Level is incorrect format`);
+        }
+
+        let newJsonFormat = [];
+
+        // Get keys level and orderby desc
+        const levels = Object.keys(payload).map(lv => +lv);
+        levels.sort((a, b) => b - a);
+
+        levels.forEach(level => {
+          const parentList = payload[level];
+          if (newJsonFormat.length === 0) {
+            newJsonFormat.push(...parentList);
+          } else {   
+            const childrenList = newJsonFormat;
+            childrenList.forEach(child => {
+              const parent = parentList.find(p => p.id === child.parent_id);
+              if (parent) {
+                parent.children.push(child);
+              } else {
+                console.log('!!!! Error not found parent !!!!');
+                console.log('Error data : ', child);
+                throw Boom.badData(`Data id ${child.id} not found parent [patent_id: ${child.parent_id}]. Please check your data`);
+              }
+            })
+            newJsonFormat = parentList;   
+          }
+
+          if (level === 0) {
+            const cntParentId = parentList.filter(p => !!p.parent_id)
+            if (cntParentId.length > 0) {
+              console.log('!!!! Error root parent is incorrect !!!!');
+              console.log('Error data : ', cntParentId);
+              throw Boom.badData(`There are some data at level 0 is incorrect. Please check your data`);
+            }
+          }
+        })
+
+        return newJsonFormat;
+
+      }
+    });
+
+    // Path2
     // Register
     await server.register(require('@hapi/vision'));
 
@@ -26,8 +102,6 @@ const init = async () => {
       path: './public',
       context
     });
-
-    // Path2
     const getPageNav = (headerLink) => {
       let result = { first: null, last: null, next: null, prev: null };
 
@@ -82,6 +156,7 @@ const init = async () => {
           }
         }
     });
+
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 };
